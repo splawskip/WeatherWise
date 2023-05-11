f3399a6f502af33bc285eb05e20b17b39022d244
Przemysław Spławski <psplawski.dev@gmail.com>
2023-05-12 00:32:09 +0200

    Add breakpoints to JS config and finally simplify search close logic

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,14 @@ import { debounce, delegateEvent, replaceHTML, unixTimeToHumanReadable, buildErr
 const WeatherWise = new MeteoStation();
 // Build the app.
 const App = {
+  /** Gather app breakpoints. */
+  breakpoints: {
+    xs: 0,
+    sm: 320,
+    md: 768,
+    lg: 1200,
+    xl: 1600,
+  },
   /** Gather app components. */
   $: {
     /** App misc components. */
@@ -37,22 +45,23 @@ const App = {
       return '';
     }
     // Indicate that the search results has results.
+    App.$.searchView.classList.add('search--open');
     App.$.searchWrapper.classList.add('search__wrapper--has-results');
     // Loop over locations data and build search results component that is sprinkled with the location data.
     const searchResultsComponent = locations
       .map(({ name, state, country, lat, lon }) => {
         return `
           <li class="search__results-item">
-            <svg xmlns="http://www.w3.org/2000/svg" class="search__results-item-icon" viewBox="0 0 16 20">
-              <path
-                d="M8 9.925c.533 0 .992-.188 1.375-.563.383-.375.575-.829.575-1.362 0-.533-.192-.992-.575-1.375A1.876 1.876 0 0 0 8 6.05c-.533 0-.987.192-1.362.575A1.896 1.896 0 0 0 6.075 8c0 .533.188.987.563 1.362.375.375.829.563 1.362.563Zm0 7.5c2.05-1.883 3.575-3.592 4.575-5.125s1.5-2.9 1.5-4.1c0-1.85-.588-3.358-1.763-4.525C11.137 2.508 9.7 1.925 8 1.925c-1.7 0-3.133.583-4.3 1.75C2.533 4.842 1.95 6.35 1.95 8.2c0 1.2.496 2.567 1.488 4.1.991 1.533 2.512 3.242 4.562 5.125Zm0 2.125a1.13 1.13 0 0 1-.362-.062A1.04 1.04 0 0 1 7.3 19.3c-2.433-2.15-4.246-4.142-5.437-5.975C.671 11.492.075 9.783.075 8.2c0-2.483.796-4.463 2.388-5.938C4.054.787 5.9.05 8 .05s3.95.737 5.55 2.212c1.6 1.475 2.4 3.455 2.4 5.938 0 1.583-.6 3.292-1.8 5.125-1.2 1.833-3.017 3.825-5.45 5.975a.79.79 0 0 1-.312.188A1.232 1.232 0 0 1 8 19.55Z"
-              />
-            </svg>
-            <span class="search__results-item-data">
-              <h3 class="search__results-item-city">${name}</h3>
-              <p class="search__results-item-country">${state}, ${country}</p>
-            </span>
             <a href="#/weather?lat=${lat}&lon=${lon}" class="search__results-item-link" data-weather="search-results-item">
+              <svg xmlns="http://www.w3.org/2000/svg" class="search__results-item-icon" viewBox="0 0 16 20">
+                <path
+                  d="M8 9.925c.533 0 .992-.188 1.375-.563.383-.375.575-.829.575-1.362 0-.533-.192-.992-.575-1.375A1.876 1.876 0 0 0 8 6.05c-.533 0-.987.192-1.362.575A1.896 1.896 0 0 0 6.075 8c0 .533.188.987.563 1.362.375.375.829.563 1.362.563Zm0 7.5c2.05-1.883 3.575-3.592 4.575-5.125s1.5-2.9 1.5-4.1c0-1.85-.588-3.358-1.763-4.525C11.137 2.508 9.7 1.925 8 1.925c-1.7 0-3.133.583-4.3 1.75C2.533 4.842 1.95 6.35 1.95 8.2c0 1.2.496 2.567 1.488 4.1.991 1.533 2.512 3.242 4.562 5.125Zm0 2.125a1.13 1.13 0 0 1-.362-.062A1.04 1.04 0 0 1 7.3 19.3c-2.433-2.15-4.246-4.142-5.437-5.975C.671 11.492.075 9.783.075 8.2c0-2.483.796-4.463 2.388-5.938C4.054.787 5.9.05 8 .05s3.95.737 5.55 2.212c1.6 1.475 2.4 3.455 2.4 5.938 0 1.583-.6 3.292-1.8 5.125-1.2 1.833-3.017 3.825-5.45 5.975a.79.79 0 0 1-.312.188A1.232 1.232 0 0 1 8 19.55Z"
+                />
+              </svg>
+              <span class="search__results-item-data">
+                <h3 class="search__results-item-city">${name}</h3>
+                <p class="search__results-item-country">${state}, ${country}</p>
+              </span>
             </a>
           </li>
         `;
@@ -593,7 +602,7 @@ const App = {
       replaceHTML(App.$.searchResults, '');
     });
     /**
-     * Clears and closes the search on escape hit.
+     * Clears the search on escape hit.
      */
     App.$.app.addEventListener('keyup', (event) => {
       if (
@@ -605,32 +614,16 @@ const App = {
       }
     });
     /**
-     * Clears and closes the search on search results item selection.
+     * Clears the search on search results item selection.
      */
-    delegateEvent(App.$.searchView, '[data-weather="search-results-item"]', 'click', (event) => {
-      // Prevent from default behavior.
-      event.preventDefault();
-      // Change the hash.
-      window.location.hash = event.target.hash ?? '';
-      // Bail search.
-      App.clearSearch();
-    });
+    delegateEvent(App.$.searchView, '[data-weather="search-results-item"]', 'click', App.clearSearch);
     /**
-     * Clears and closes the search on focusout (blur does not bubble).
+     * Clears the search on click outside search element on desktop.
      */
-    document.addEventListener('focusout', (event) => {
-      // Get element that is responsible for focusout event.
-      const elementThatCausedFocusout = event.relatedTarget;
-      // If element that caused focusout event is search results item or search-input, escape the callback.
-      if (
-        !(elementThatCausedFocusout instanceof Element) ||
-        elementThatCausedFocusout.matches('[data-weather="search-results-item"]') ||
-        elementThatCausedFocusout.matches('[data-weather="search-input"]')
-      ) {
-        return;
+    document.addEventListener('click', (event) => {
+      if (window.innerWidth >= App.breakpoints.lg && !App.$.searchView.contains(event.target)) {
+        App.clearSearch();
       }
-      // Bail search if some other element caused focusout event.
-      App.clearSearch();
     });
   },
   /**
