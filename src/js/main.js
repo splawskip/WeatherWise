70df1a46d812929a6ae65ca2f31e31c1943e2669
Przemysław Spławski <psplawski.dev@gmail.com>
2023-05-22 13:18:50 +0200

    Add open animation to search component

--- src/js/main.js
+++ src/js/main.js
@@ -17,7 +17,7 @@ const App = {
   $: {
     /** App misc components. */
     body: document.body,
-    app: document.querySelector('[data-weather="app"]'),
+    appContainer: document.querySelector('[data-weather="app"]'),
     content: document.querySelector('[data-weather="content"]'),
     loader: document.querySelector('[data-weather="loader"]'),
     errorPopup: document.querySelector('[data-weather="error-popup"]'),
@@ -37,32 +37,36 @@ const App = {
   buildSearchResultsComponent(locations) {
     // Early return if there are not any locations.
     if (!locations.length) {
-      App.$.searchWrapper.classList.remove('search__wrapper--has-results');
+      App.$.searchView.classList.remove('search--has-results');
       return '';
     }
     // Indicate that the search results has results.
     App.$.searchView.classList.add('search--open');
-    App.$.searchWrapper.classList.add('search__wrapper--has-results');
+    App.$.searchView.classList.add('search--has-results');
     // Loop over locations data and build search results component that is sprinkled with the location data.
-    const searchResultsComponent = locations
-      .map(({ name, state, country, lat, lon }) => {
-        return `
-          <li class="search__results-item">
-            <a href="#/weather?lat=${lat}&lon=${lon}" class="search__results-item-link" data-weather="search-results-item">
-              <svg xmlns="http://www.w3.org/2000/svg" class="search__results-item-icon" viewBox="0 0 16 20">
-                <path
-                  d="M8 9.925c.533 0 .992-.188 1.375-.563.383-.375.575-.829.575-1.362 0-.533-.192-.992-.575-1.375A1.876 1.876 0 0 0 8 6.05c-.533 0-.987.192-1.362.575A1.896 1.896 0 0 0 6.075 8c0 .533.188.987.563 1.362.375.375.829.563 1.362.563Zm0 7.5c2.05-1.883 3.575-3.592 4.575-5.125s1.5-2.9 1.5-4.1c0-1.85-.588-3.358-1.763-4.525C11.137 2.508 9.7 1.925 8 1.925c-1.7 0-3.133.583-4.3 1.75C2.533 4.842 1.95 6.35 1.95 8.2c0 1.2.496 2.567 1.488 4.1.991 1.533 2.512 3.242 4.562 5.125Zm0 2.125a1.13 1.13 0 0 1-.362-.062A1.04 1.04 0 0 1 7.3 19.3c-2.433-2.15-4.246-4.142-5.437-5.975C.671 11.492.075 9.783.075 8.2c0-2.483.796-4.463 2.388-5.938C4.054.787 5.9.05 8 .05s3.95.737 5.55 2.212c1.6 1.475 2.4 3.455 2.4 5.938 0 1.583-.6 3.292-1.8 5.125-1.2 1.833-3.017 3.825-5.45 5.975a.79.79 0 0 1-.312.188A1.232 1.232 0 0 1 8 19.55Z"
-                />
-              </svg>
-              <span class="search__results-item-data">
-                <h3 class="search__results-item-city">${name}</h3>
-                <p class="search__results-item-country">${state}, ${country}</p>
-              </span>
-            </a>
-          </li>
-        `;
-      })
-      .join('');
+    const searchResultsComponent = `
+      <ul class="search__results-list" data-weather="search-results-list">
+        ${locations
+          .map(({ name, state, country, lat, lon }) => {
+            return `
+              <li class="search__results-item">
+                <a href="#/weather?lat=${lat}&lon=${lon}" class="search__results-item-link" data-weather="search-results-item">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="search__results-item-icon" viewBox="0 0 16 20">
+                    <path
+                      d="M8 9.925c.533 0 .992-.188 1.375-.563.383-.375.575-.829.575-1.362 0-.533-.192-.992-.575-1.375A1.876 1.876 0 0 0 8 6.05c-.533 0-.987.192-1.362.575A1.896 1.896 0 0 0 6.075 8c0 .533.188.987.563 1.362.375.375.829.563 1.362.563Zm0 7.5c2.05-1.883 3.575-3.592 4.575-5.125s1.5-2.9 1.5-4.1c0-1.85-.588-3.358-1.763-4.525C11.137 2.508 9.7 1.925 8 1.925c-1.7 0-3.133.583-4.3 1.75C2.533 4.842 1.95 6.35 1.95 8.2c0 1.2.496 2.567 1.488 4.1.991 1.533 2.512 3.242 4.562 5.125Zm0 2.125a1.13 1.13 0 0 1-.362-.062A1.04 1.04 0 0 1 7.3 19.3c-2.433-2.15-4.246-4.142-5.437-5.975C.671 11.492.075 9.783.075 8.2c0-2.483.796-4.463 2.388-5.938C4.054.787 5.9.05 8 .05s3.95.737 5.55 2.212c1.6 1.475 2.4 3.455 2.4 5.938 0 1.583-.6 3.292-1.8 5.125-1.2 1.833-3.017 3.825-5.45 5.975a.79.79 0 0 1-.312.188A1.232 1.232 0 0 1 8 19.55Z"
+                    />
+                  </svg>
+                  <span class="search__results-item-data">
+                    <h3 class="search__results-item-city">${name}</h3>
+                    <p class="search__results-item-country">${state}, ${country}</p>
+                  </span>
+                </a>
+              </li>
+            `;
+          })
+          .join('')}
+      </ul>
+    `;
     // Return component as HTML string.
     return searchResultsComponent;
   },
@@ -130,10 +134,17 @@ const App = {
    */
   buildForecastComponent(forecast) {
     // Build forecast component.
-    const forecastComponent = forecast
-      .filter((_, index) => (index + 1) % 8 === 0)
-      .map(({ weather: [{ icon, description }], dt: dateUnix, main: { temp_max: tempMax } }) => {
-        return `
+    const forecastComponent = `
+      <!-- Five days forecast section -->
+      <section class="section forecast-section">
+        <h3 class="title section__title forecast-section__title">
+          5 days forecast
+        </h3>
+        <section class="forecast-section__card forecast-card" data-weather="forecast-section">
+          ${forecast
+            .filter((_, index) => (index + 1) % 8 === 0)
+            .map(({ weather: [{ icon, description }], dt: dateUnix, main: { temp_max: tempMax } }) => {
+              return `
               <div class="forecast-card__day-forecast">
                 <p class="forecast-card__temperatures">
                   <img loading="lazy" src="./icons/weather/${icon}-mobile.webp" srcset="./icons/weather/${icon}-mobile.webp 32w, ./icons/weather/${icon}-desktop.webp 48w" sizes="(min-width: 1200px) 48px, 32px" title="${description}" alt="${description}" class="forecast-card__icon" />
@@ -156,19 +167,13 @@ const App = {
                 </p>
               </div>
             `;
-      })
-      .join('');
-    // Return component as HTML string.
-    return `
-      <!-- Five days forecast section -->
-      <section class="section forecast-section">
-        <h3 class="title section__title forecast-section__title">
-          5 days forecast
-        </h3>
-        <section class="forecast-section__card forecast-card" data-weather="forecast-section">
-          ${forecastComponent}
+            })
+            .join('')}
         </section>
-      </section>`;
+      </section>
+      `;
+    // Return component as HTML string.
+    return forecastComponent;
   },
   /**
    * Builds the air quality component sprinkled with air quality data.
@@ -396,7 +401,7 @@ const App = {
               class="today-at-card__icon"
             />
             <p class="today-at-card__label">${parseInt(temp, 10)}${getHTMLEntity('celsiusDegree')}</p>
-         </li>
+          </li>
         `;
       })
       .join('');
@@ -414,28 +419,28 @@ const App = {
     const followingHoursWindCards = forecast
       .map(({ dt: dateUnix, wind: { deg, speed } }) => {
         return `
-        <li class="today-at-card">
-          <time class="today-at-card__label">
-            ${unixTimeToHumanReadable(dateUnix, {
-              hour: '2-digit',
-              hour12: true,
-            })}
-          </time>
-          <img
-            loading="lazy"
-            src="./icons/wind-direction-mobile.webp"
-            srcset="
-              ./icons/wind-direction-mobile.webp 32w,
-              ./icons/wind-direction-desktop.webp 48w
-            "
-            sizes="(min-width: 1200px) 48px, 32px"
-            alt=""
-            class="today-at-card__icon"
-            style="transform: rotate(${deg}deg);"
-          />
-          <p class="today-at-card__label">${speed} km/h</p>
-        </li>
-      `;
+          <li class="today-at-card">
+            <time class="today-at-card__label">
+              ${unixTimeToHumanReadable(dateUnix, {
+                hour: '2-digit',
+                hour12: true,
+              })}
+            </time>
+            <img
+              loading="lazy"
+              src="./icons/wind-direction-mobile.webp"
+              srcset="
+                ./icons/wind-direction-mobile.webp 32w,
+                ./icons/wind-direction-desktop.webp 48w
+              "
+              sizes="(min-width: 1200px) 48px, 32px"
+              alt=""
+              class="today-at-card__icon"
+              style="transform: rotate(${deg}deg);"
+            />
+            <p class="today-at-card__label">${speed} km/h</p>
+          </li>
+        `;
       })
       .join('');
     // Return component as HTML string.
@@ -627,7 +632,7 @@ const App = {
     /**
      * Toggles search state on search toggle is click.
      */
-    delegateEvent(App.$.app, '[data-weather="search-toggle"]', 'click', () => {
+    delegateEvent(App.$.appContainer, '[data-weather="search-toggle"]', 'click', () => {
       App.$.searchView.classList.toggle('search--open');
       App.$.body.classList.toggle('body--no-scroll');
       App.$.search.value = '';
@@ -644,7 +649,7 @@ const App = {
       // Get if search has results.
       const hasResults =
         App.$.searchView.classList.contains('search--open') &&
-        App.$.searchWrapper.classList.contains('search__wrapper--has-results');
+        App.$.searchView.classList.contains('search--has-results');
       // Clear search if user want to close it using escape key.
       if (hasResults && event.key === 'Escape') {
         App.clearSearch();
@@ -675,7 +680,7 @@ const App = {
    */
   clearSearch() {
     App.$.searchView.classList.remove('search--open');
-    App.$.searchWrapper.classList.remove('search__wrapper--has-results');
+    App.$.searchView.classList.remove('search--has-results');
     App.$.body.classList.remove('body--no-scroll');
     App.$.search.value = '';
     replaceHTML(App.$.searchResults, '');
